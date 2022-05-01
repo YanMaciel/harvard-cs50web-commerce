e54bbaf55d3a9172382aeac3386ca829e5206df3
YanMaciel <araujo.yan@hotmail.com>
2022-05-01 13:10:30 -0300

    feat: redo models

--- auctions/models.py
+++ auctions/models.py
@@ -1,36 +1,44 @@
 from unicodedata import category
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
 
-class Item(models.Model):
+class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=254)
     image_url = models.CharField(max_length=600)
     category = models.CharField(max_length=64)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_listings")
+    created_date = models.DateTimeField(default=timezone.now)
+    starting_price = models.FloatField()
+    current_bid = models.FloatField(blank=True, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator_listings")
+    watchers = models.ManyToManyField(User, blank=True, related_name="watched_listings")
+    buyer = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
     
     def __str__(self):
-        return f"Item of ID {self.id}, name: {self.title} and owner: {self.owner.username}."
+        return f"{self.title} - {self.starting_bid}"
     
 class Comment(models.Model):
-    comment = models.CharField(max_length=254)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_comments")
-    product = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="product_comments")
+    comment = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="product_comments")
     
     def __str__(self):
-        return f"Comment of ID {self.id}, product: {self.product} and owner: {self.owner.username}."
+        return self.createdDate.strftime('%B %d %Y')
     
 class Bid(models.Model):
-    bid_price = models.FloatField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_bids")
-    product = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="product_bids")
+    offer = models.FloatField()
+    created_date = models.DateTimeField(default=timezone.now)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Listing, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f"Bid of ID {self.id} and price ${self.bid_price}, product: {self.product} and owner: {self.owner.username}."
+        return f"{self.offer} for listing {self.product}"
     
     
     
--- auctions/views.py
+++ auctions/views.py
@@ -2,20 +2,17 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-from .models import User, Item, Bid
+from .models import User, Listing, Bid
 
 
 def index(request):
-    active_listings = Item.objects.all()
-    current_prices = Bid.objects.order_by().values_list('product').distinct()
-    print(current_prices)
+    active_listings = Listing.objects.all()
     return render(request, "auctions/index.html", {
-        "active_listings": active_listings,
-        "current_prices": current_prices
+        "active_listings": active_listings
     })
 
 
@@ -74,37 +71,33 @@ def register(request):
 def create_listing(request):
     if request.method == "POST":
         if request.POST["price"].isdigit():
-            listing = Item(
+            listing = Listing(
                 title=request.POST["title"],
                 description=request.POST["description"],
                 image_url=request.POST["image_url"],
                 category=request.POST["category"],
-                owner=User(id=request.user.id)
+                starting_price=float(request.POST["price"]),
+                creator=User(id=request.user.id)
             )
             listing.save()
             
-            start_price = Bid(
-                bid_price=float(request.POST["price"]),
-                owner=User(id=request.user.id),
-                product=Item(id=listing.id)
-            )
-            start_price.save()
-            print(Bid.objects.filter(id=start_price.id))
+            return redirect("listings", listing_id=listing.id)
+            
         else:
             return render(request, "auctions/create_listing.html", {
                 "error": "error"
             })
     
-    # print(Item.objects.filter(owner=request.user.id))
     return render(request, "auctions/create_listing.html")
 
 def listings(request, listing_id):
     
-    listing = Item.objects.get(id=listing_id)
-    current_price = Bid.objects.filter(id=listing_id).latest('id')
+    if not request.user.is_authenticated:
+        return redirect("login")
+    
+    listing = Listing.objects.get(id=listing_id)
     
     return render(request, "auctions/listing.html", {
         "listing": listing,
-        "current_price": current_price
     })
     
